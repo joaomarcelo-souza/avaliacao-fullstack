c80e6e1f7d1f13e2517dc88646897b505d07912a
João Souza <marceloamaral201924@gmail.com>
2026-02-03 16:31:43 -0400

    feat: adding tag to users controllers for better documentation

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -6,6 +6,7 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { ResponseUserDto } from './dto/response-user.dto';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
