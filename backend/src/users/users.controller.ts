90984739d0643618af2410656ef134640109e564
João Souza <marceloamaral201924@gmail.com>
2026-02-03 16:35:37 -0400

    feat: new get endpoint '/users/:id' that gets an user by id

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -36,8 +36,11 @@ export class UsersController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  @ApiOperation({ summary: 'Get a user by id' })
+  @ApiOkResponse({ type: ResponseUserDto })
+  @ApiNotFoundResponse()
+  findOne(@Param('id', ParseIntPipe) id: number): Promise<ResponseUserDto> {
+    return this.usersService.findOne(id);
   }
 
   @Patch(':id')
