44676d84c4ce522d207cb31c759c28f197df95de
João Souza <marceloamaral201924@gmail.com>
2026-02-04 13:52:09 -0400

    feat: adding the service provider to config

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,9 +4,18 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import localePtExtra from '@angular/common/locales/extra/pt';
+import { userServiceProvider } from './users/services/user.provider.service';
+
+registerLocaleData(localePt, 'pt-BR', localePtExtra);
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    userServiceProvider,
+  ],
 };
